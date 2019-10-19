33433633342fa828c9601e9f43a656859b1d6b2b
Anna Kulay <annakulay10@gmail.com>
2019-10-19 15:57:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function multiply(first, second) {
-  return String(BigInt(first) * BigInt(second));
+  if (first === 0 || second === 0) {
+    return '0';
+  }
+
+  const firstNumber = first.split('').reverse();
+  const secondNumber = second.split('').reverse();
+  const result = new Array(first.length + second.length).fill(null);
+
+  for (let i = 0; i < firstNumber.length; i++) {
+    for (let j = 0; j < secondNumber.length; j++) {
+      result[i + j] += firstNumber[i] * secondNumber[j];
+    }
+  }
+
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] >= 10) {
+      if (!result[i + 1]) {
+        result[i + 1] = null;
+      }
+      result[i + 1] += Math.trunc(result[i] / 10);
+      result[i] %= 10;
+    }
+  }
+
+  return result.reverse().join('');
 }
